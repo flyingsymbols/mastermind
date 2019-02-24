c20ae857576b3410df23386f1ccdca03aee2509a
flyingsymbols <david.hogarty@gmail.com>
2019-02-23 19:19:54 -0500

    working on getting this more efficient

--- mastermind.py
+++ mastermind.py
@@ -1,6 +1,7 @@
-from columnize import columnize
-
+import math
 from collections import namedtuple, defaultdict
+
+from columnize import columnize
 
 COLORS = [
     'W', # white
@@ -61,14 +62,6 @@ class Answer(namedtuple('Answer', ['guess', 'result'])):
             self.result[0], self.result[1]
         )
 
-ANSWERS = [
-    Answer(colors_to_num('OOGP'), (0,1)),
-    Answer(colors_to_num('RYWW'), (1,2)),
-    Answer(colors_to_num('PRPR'), (1,0)),
-    Answer(colors_to_num('GRYW'), (1,1)),
-    Answer(colors_to_num('WRWO'), (0,2)),
-    Answer(colors_to_num('YWPW'), (4,0))
-]
 
 def consistent(i, answer):
     return match(i, answer.guess) == answer.result
@@ -89,11 +82,66 @@ def next_partitions(answers, i):
     The idea behind this is to, for a given set of answers, and a potential next guess i,
     return the partitions {(red, white): set(nums)}
     """
-    parts = {}
+    parts = defaultdict(set)
     for num in yield_consistent(answers):
         res = match(i, num)
+        parts[res].add(num)
+    return parts
+
+def minmax_metric(parts):
+    return -max(len(v) for v in parts.values())
+
+def best_partitions(prev_answers, metric=minmax_metric):
+    """
+    Takes the previous_answers, returns a set of values that maximize the given metric
+
+    metric(answer_partitions) -> value
+    """
+    best_val = -math.inf
+    values = []
+    for i in range(6**4):
+        parts = next_partitions(prev_answers, i)
+        metric_val = metric(parts)
+        if metric_val > best_val:
+            best_val = metric_val
+            values = [i]
+        elif metric_val == best_val:
+            values.append(i)
+        # else metric_val < best_val so we can ignore
+    return best_val, values
 
 def main():
+    cur_answers = []
+    answer_tree = {}
+
+    metric_val, best_next = best_partitions(cur_answers, minmax_metric)
+    print('After {}, {} with value {}:'.format(cur_answers, len(best_next), metric_val))
+    print(columnize(
+        [num_to_colors(v) for v in best_next],
+        displaywidth=80
+    ))
+
+    cur_answer = best_next[0]
+    answer_tree[cur_answer] = {}
+
+    next_parts = next_partitions([], cur_answer)
+    for res, values in next_parts.items():
+        print('|{}| = {}'.format(Answer(cur_answer, res), len(values)))
+        print(columnize(
+            [num_to_colors(v) for v in values],
+            displaywidth=80
+        ))
+
+def analyze_game_20190222():
+    ANSWERS = [
+        Answer(colors_to_num('OOGP'), (0,1)),
+        Answer(colors_to_num('RYWW'), (1,2)),
+        Answer(colors_to_num('PRPR'), (1,0)),
+        Answer(colors_to_num('GRYW'), (1,1)),
+        Answer(colors_to_num('WRWO'), (0,2)),
+        Answer(colors_to_num('YWPW'), (4,0))
+    ]
+
     for i in range(1, len(ANSWERS) + 1):
         answers = ANSWERS[:i]
         possible = count_consistent(answers)
